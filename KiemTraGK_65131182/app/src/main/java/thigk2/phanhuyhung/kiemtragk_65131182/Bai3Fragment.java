9aabbfd7a463ea5a7cc02862b30d1edc29a990ae
Phan Huy Hung <hunghuy0925@gmail.com>
2026-04-20 08:21:42 +0700

    COMMIT LẦN THỨ 4

--- KiemTraGK_65131182/app/src/main/java/thigk2/phanhuyhung/kiemtragk_65131182/Bai3Fragment.java
+++ KiemTraGK_65131182/app/src/main/java/thigk2/phanhuyhung/kiemtragk_65131182/Bai3Fragment.java
@@ -32,8 +32,10 @@ public class Bai3Fragment extends Fragment {
         // Tạo danh sách địa điểm du lịch
         List<Attraction> attractions = new ArrayList<>();
         attractions.add(new Attraction("Tháp Nha Trang", "Nha Trang, Khánh Hòa", R.drawable.thap_nha_trang));
-        attractions.add(new Attraction("Bãi Biển Nha Trang", "Phường Vĩnh Phương, Nha Trang", R.drawable.bai_bien_nha_trang));
-        attractions.add(new Attraction("Vinpearl Amusement Park", "Đảo Hòn Tre, Nha Trang", R.drawable.winpeal_nha_trang));
+        attractions.add(
+                new Attraction("Bãi Biển Nha Trang", "Phường Vĩnh Phương, Nha Trang", R.drawable.bai_bien_nha_trang));
+        attractions
+                .add(new Attraction("Vinpearl Amusement Park", "Đảo Hòn Tre, Nha Trang", R.drawable.winpeal_nha_trang));
         attractions.add(new Attraction("Đảo Mộc Châu", "Vịnh Nha Trang, Khánh Hòa", R.drawable.dao_moc_chau));
         attractions.add(new Attraction("Chùa Lâm Ty Ni", "Thôn Khánh Phong, Nha Trang", R.drawable.chua_lam_ty_ni));
         attractions.add(new Attraction("Suối Tranh", "Khánh Sơn, Khánh Hòa", R.drawable.suoi_tranh));
